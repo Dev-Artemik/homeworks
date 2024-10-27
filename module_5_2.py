e73a7c4938c2fa18949f32eef55c91ed3628af03
Artem <artemknovv@yandex.ru>
2024-10-27 18:50:37 +0300

    Sync 5.3!

--- module_5_2.py
+++ module_5_2.py
@@ -20,10 +20,23 @@ class House:
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
-# __str__
 print(h1)
 print(h2)
 
-# __len__
-print(len(h1))
-print(len(h2))
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
